b04d0e9d0877aba0b9aa517afdc9f8622bf6a021
Alexander <107309506+snrtro@users.noreply.github.com>
2023-07-28 20:47:15 +0500

    Initial commit

---
+++ source/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+
+var folder = @"C:\Users\alexander\Desktop\testData";
+
+
+Directory.GetFiles(folder)
+    .Select(f =>
+    {
+        using var fs = new FileStream(f, FileMode.Open, FileAccess.Read);
+        return new
+        {
+            Filename = f,
+            Filehash = BitConverter.ToString(SHA1.Create().ComputeHash(fs))
+        };
+    })
+    .GroupBy(f => f.Filehash)
+    .Select(g => new { Filehash = g.Key, Files = g.Select(z => z.Filename).ToList() })
+    .SelectMany(f => f.Files.Skip(1))
+    .ToList()
+    .ForEach(File.Delete);
+
+Console.WriteLine("DONE!");
